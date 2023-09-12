88c9cd7540fbf0fe0bb5230978be93590c52f643
19992018 <margretmauno16@gmail.com>
2023-09-12 13:25:39 +0300

    Added event.preventDefault to market.js

--- market.js
+++ market.js
@@ -8,84 +8,85 @@ const gallery = document.querySelectorAll(".gallery img");
 
 //To make the y scroll appear
 //We had removed y scroll due to the splash screen
-document.addEventListener('DOMContentLoaded', (e)=>{
-    body.style.overflow = "auto";
+document.addEventListener('DOMContentLoaded', (e) => {
+  body.style.overflow = "auto";
 
 })
 
-document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener("scroll", function() {
-      var scroll = window.scrollTop || document.documentElement.scrollTop;
-      var transparentElement = document.querySelector(".transparent");
-  
-      if (scroll > 19) {
-        transparentElement.style.background = "black";
-      } else {
-        transparentElement.style.background = "transparent";
-      }
-    });
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    var scroll = window.scrollTop || document.documentElement.scrollTop;
+    var transparentElement = document.querySelector(".transparent");
 
-
-  //Events-Filter JS
-$(document).ready(function(){
-  $(".filter-item").click(function(){
-      const value = $(this).attr("data-filter");
-      if(value == 'all'){
-          $(".gallery").show("1000");
-      }else{
-          $(".gallery")
-          .not("."+ value)
-          .hide("1000");
-          $(".gallery")
-          .filter("."+ value)
-          .show("1000");
-      }
+    if (scroll > 19) {
+      transparentElement.style.background = "black";
+    } else {
+      transparentElement.style.background = "transparent";
+    }
   });
-//Add Active to btn
-$(".filter-item").click(function(){
-  $(this).addClass("active-filter").siblings().removeClass("active-filter");
 });
+
+
+//Events-Filter JS
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == 'all') {
+      $(".gallery").show("1000");
+    } else {
+      $(".gallery")
+        .not("." + value)
+        .hide("1000");
+      $(".gallery")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+  //Add Active to btn
+  $(".filter-item").click(function () {
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
 });
 
 
 
 //To open single-event.html on clicking gallery div
-for(var i = 0, length = gallery.length; i < length; i++) {
-      gallery[i].onclick = function(){
-      location.href = "single-event.html";
+for (var i = 0, length = gallery.length; i < length; i++) {
+  gallery[i].onclick = function (event) {
+    event.preventDefault();
+    location.href = "single-event.html";
   }
-  } 
+}
 
-  
+
 // MODAL BOX FUNCTIONALITY
 // When the user clicks on the button, open the modal
 
 for (let i = 0; i < btn2.length; i++) {
-  btn2[i].onclick = function() {
-  modal.style.display = "flex";
+  btn2[i].onclick = function () {
+    modal.style.display = "flex";
     modal.style.flexDirection = "column";
     modal.style.justifyContent = "center";
-    modal.style.alignItems= "center";
-}
+    modal.style.alignItems = "center";
+  }
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-modal.style.display = "none";
+span.onclick = function () {
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == modal) {
-  modal.style.display = "none";
-}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 
 // for all some page buttons to lead to single-event page.
 for (let i = 0; i < btn.length; i++) {
-  btn[i].onclick = function() {
-  location.href = "single-event.html";
-}
+  btn[i].onclick = function () {
+    location.href = "single-event.html";
+  }
 }
